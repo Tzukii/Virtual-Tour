6dd72f632abdfbab7dbf87e7380ccf65669f07ef
Shivam Maji <70868901+ParadoxFlame@users.noreply.github.com>
2022-07-26 17:46:29 -0700

    Improved Select Animation + Zoom In/Out

--- js/Tour.js
+++ js/Tour.js
@@ -1,22 +1,44 @@
 AFRAME.registerComponent("tour", {
-    init: function () {
-        this.placesContainer = this.el;
-        this.createCards();
-    },
-
     schema: {
         state: { type: "string", default: "places-list" },
         selectedCard: { type: "string", default: "#card1" },
+        zoomAspectRatio: { type: "number", default: 1 }
     },
-
+    init: function () {
+        this.placesContainer = this.el;
+        this.cameraEl = document.querySelector("#camera");
+        this.createCards();
+    },
+    update: function () {
+        window.addEventListener("keydown", e => {
+            if (e.key === "ArrowUp") {
+                if (
+                    (this.data.zoomAspectRatio <= 10 && this.data.state === "view") ||
+                    (this.data.zoomAspectRatio <= 10 && this.data.state === "change-view")
+                ) {
+                    this.data.zoomAspectRatio += 0.002;
+                    this.cameraEl.setAttribute("zoom", this.data.zoomAspectRatio);
+                }
+            }
+            if (e.key === "ArrowDown") {
+                if (
+                    (this.data.zoomAspectRatio > 1 && this.data.state === "view") ||
+                    (this.data.zoomAspectRatio > 1 && this.data.state === "change-view")
+                ) {
+                    this.data.zoomAspectRatio -= 0.002;
+                    this.cameraEl.setAttribute("zoom", this.data.zoomAspectRatio);
+                }
+            }
+        });
+    },
     tick: function () {
         const { state } = this.el.getAttribute("tour");
+
         if (state === "view") {
             this.hideEl([this.placesContainer]);
             this.showView();
         }
     },
-
     hideEl: function (elList) {
         elList.map(el => {
             el.setAttribute("visible", false);
@@ -25,41 +47,40 @@ AFRAME.registerComponent("tour", {
     showView: function () {
         const { selectedCard } = this.data;
 
+        //Set the 360 degree image to the sky element.
         const skyEl = document.querySelector("#main-container");
 
         skyEl.setAttribute("material", {
             src: `./assets/360_images/${selectedCard}/place-0.jpg`,
-            color: "white"
-        })
+            color: "#fff"
+        });
     },
-
     createCards: function () {
+
         const thumbNailsRef = [
             {
                 id: "taj-mahal",
                 title: "Taj Mahal",
-                url: "./assets/thumbnails/taj_mahal.png",
+                url: "./assets/thumbnails/taj_mahal.png"
             },
             {
                 id: "budapest",
                 title: "Budapest",
-                url: "./assets/thumbnails/budapest.jpg",
+                url: "./assets/thumbnails/budapest.jpg"
             },
 
             {
                 id: "eiffel-tower",
                 title: "Eiffel Tower",
-                url: "./assets/thumbnails/eiffel_tower.jpg",
+                url: "./assets/thumbnails/eiffel_tower.jpg"
             },
             {
                 id: "new-york-city",
                 title: "New York City",
-                url: "./assets/thumbnails/new_york_city.png",
-            },
+                url: "./assets/thumbnails/new_york_city.png"
+            }
         ];
-
         let prevoiusXPosition = -60;
-
         for (var item of thumbNailsRef) {
             const posX = prevoiusXPosition + 25;
             const posY = 10;
@@ -70,7 +91,7 @@ AFRAME.registerComponent("tour", {
             // Border Element
             const borderEl = this.createBorder(position, item.id);
 
-            // Thumbnail Element
+            // Thubnail Element
             const thumbNail = this.createThumbNail(item);
             borderEl.appendChild(thumbNail);
 
@@ -88,16 +109,14 @@ AFRAME.registerComponent("tour", {
         entityEl.setAttribute("geometry", {
             primitive: "ring",
             radiusInner: 9,
-            radiusOuter: 10,
+            radiusOuter: 10
         });
         entityEl.setAttribute("position", position);
         entityEl.setAttribute("material", {
             color: "#0077CC",
-            opacity: 1,
+            opacity: 1
         });
-
         entityEl.setAttribute("cursor-listener", {});
-
         return entityEl;
     },
     createThumbNail: function (item) {
@@ -105,10 +124,10 @@ AFRAME.registerComponent("tour", {
         entityEl.setAttribute("visible", true);
         entityEl.setAttribute("geometry", {
             primitive: "circle",
-            radius: 9,
+            radius: 9
         });
         entityEl.setAttribute("material", { src: item.url });
-
+        entityEl.setAttribute("cursor-listener", {});
         return entityEl;
     },
     createTitleEl: function (position, item) {
@@ -116,9 +135,9 @@ AFRAME.registerComponent("tour", {
         entityEl.setAttribute("text", {
             font: "exo2bold",
             align: "center",
-            width: 70,
+            width: 60,
             color: "#e65100",
-            value: item.title,
+            value: item.title
         });
         const elPosition = position;
         elPosition.y = -20;
@@ -126,4 +145,5 @@ AFRAME.registerComponent("tour", {
         entityEl.setAttribute("visible", true);
         return entityEl;
     },
+
 });
